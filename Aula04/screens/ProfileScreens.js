b0e71666d280fa604d8e08061619bd2f54c44944
user0101000 <blancaribeirosilva72@gmail.com>
2025-09-03 01:06:05 +0000

    (02/09) Atividade Aula 09

---
+++ Aula04/screens/ProfileScreens.js
@@ -0,0 +1,22 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Tela de Perfil</Text>
+      <Button title="Voltar" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titulo: {
+    fontSize: 24,
+    color: '#007AFF',
+  },
+});
