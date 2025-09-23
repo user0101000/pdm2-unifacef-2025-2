f33b93b0dab53867b0469f93a5b9f27d67703c92
user0101000 <blancaribeirosilva72@gmail.com>
2025-09-23 23:01:08 +0000

    Prova1

---
+++ Prova1/App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+         <Stack.Screen name="Início" component={HomeScreen} />
+         <Stack.Screen name="Detalhes" component={DetailsScreen} />
+       </Stack.Navigator>
+     </NavigationContainer>
+   );
+}
