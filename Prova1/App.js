8db6965d95993345f4eb645eac090eec91a61cf7
user0101000 <blancaribeirosilva72@gmail.com>
2025-11-27 17:39:16 +0000

    correção_Prova1

--- Prova1/App.js
+++ Prova1/App.js
@@ -1,6 +1,9 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
+import WebImageScreen from './screens/WebImageScreen';
+import LocalImageScreen from './screens/LocalImageScreen'
+import IconsScreen from './screens/IconsScreen'
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Início" component={HomeScreen} />
-        <Stack.Screen name="Imagens da web" component={WebImageScreen} />
+        <Stack.Screen name="Imagem da Internet" component={WebImageScreen} />
         <Stack.Screen name="Imagem local" component={LocalImageScreen} />
         <Stack.Screen name="Ícones" component={IconsScreen} />
       </Stack.Navigator>
