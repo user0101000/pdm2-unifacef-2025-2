fd353621e9860f2ed578aa5a2add67b7028a7eae
user0101000 <blancaribeirosilva72@gmail.com>
2025-11-27 17:05:01 +0000

    correção_Prova2

--- Prova2/App.js
+++ Prova2/App.js
@@ -1,8 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
-import CompromissosDia from './screens/CompromissosDia';
-import CompromissosSemana from './screens/CompromissosSemana';    
+import DayScreen from './screens/DayScreen';
+import WeekScreen from './screens/WeekScreen';    
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +11,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Início" component={HomeScreen} />
-        <Stack.Screen name="COMPROMISSOS DO DIA" component={CompromissosDia} />
-        <Stack.Screen name="COMPROMISSOS DA SEMANA" component={CompromissosSemana} />
+        <Stack.Screen name="Compromissos do dia" component={DayScreen} />
+        <Stack.Screen name="Compromissos da semana" component={WeekScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Prova2/screens/HomeScreen.js
+++ Prova2/screens/HomeScreen.js
@@ -1,10 +1,12 @@
-import {View, Text, Button, StyleSheet} from 'react-native'
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function HomeScreen({ navigation }) {  
-  return <>
+export default function HomeScreen({ navigation }) {
+  return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Agenda</Text>
-      <Text style={styles.subtitulo}>Blanca Ribeiro Silva - 8º Semestre de Ciência da Computação</Text>
+      <Text style={styles.title}>Agenda</Text>
+      <Text style={styles.ident}>Blanca Ribeiro Silva</Text>
+      <Text style={styles.ident}>8º Semestre de Ciência da Computação</Text>
+      <View style={styles.spacing}></View>
       <Button
         title="Compromissos do dia"
         onPress={() => navigation.navigate('Compromissos do dia')}
@@ -14,10 +16,16 @@ export default function HomeScreen({ navigation }) {
         onPress={() => navigation.navigate('Compromissos da semana')}
       />
     </View>
-  </>
+  );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  titulo: { fontSize: 24 }, subtitulo: { fontSize: 16 }
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  ident: {
+    textAlign: 'center'
+  },
+  spacing: {
+    marginBottom: 50
+  }
 });
